b925ed3254172fea30e8cc1bee0ab20781bdef21
blancgoat <blancgoat@gmail.com>
2022-02-07 20:39:06 +0900

    누가 컨트롤러에 트랜잭션을 넣었지 - 상엽

--- src/main/java/io/blancgoat/board/question/QuestionController.java
+++ src/main/java/io/blancgoat/board/question/QuestionController.java
@@ -4,8 +4,6 @@ import io.blancgoat.board.question.dto.QuestionDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/question")
@@ -23,19 +21,16 @@ public class QuestionController {
         return questionService.getQuestion(id);
     }
 
-    @Transactional
     @PostMapping
     public Question postQuestion(@RequestBody QuestionDto.WritePostReq req) {
         return questionService.createQuestion(req);
     }
 
-    @Transactional
     @PatchMapping("/{id}")
     public void patchQuestion(@PathVariable Long id, @RequestBody QuestionDto.WritePatchReq req) {
         questionService.updateQuestion(id, req);
     }
 
-    @Transactional
     @DeleteMapping("/{id}")
     public void deleteQuestion(@PathVariable Long id) {
         questionService.deleteQuestion(id);
--- src/main/java/io/blancgoat/board/question/QuestionService.java
+++ src/main/java/io/blancgoat/board/question/QuestionService.java
@@ -3,6 +3,7 @@ package io.blancgoat.board.question;
 import io.blancgoat.board.question.dto.QuestionDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,18 +11,22 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    @Transactional(readOnly = true)
     public Iterable<Question> getQuestions() {
         return questionRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Question getQuestion(Long id) {
         return questionRepository.findByIdAndIsDelete(id, false);
     }
 
+    @Transactional
     public Question createQuestion(QuestionDto.WritePostReq req) {
         return questionRepository.save(req.toEntity());
     }
 
+    @Transactional
     public Long updateQuestion(Long id, QuestionDto.WritePatchReq req) {
         Question question = questionRepository.findById(id).orElseThrow();
         question.updateTitleAndDescription(req.getTitle(), req.getDescription());
@@ -29,6 +34,7 @@ public class QuestionService {
         return id;
     }
 
+    @Transactional
     public void deleteQuestion(Long id) {
         Question question = questionRepository.findById(id).orElseThrow();
         question.updateIsDeleteToTrue();
